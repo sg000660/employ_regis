e0fc6d374c278b94f5850de3e8ec79a60afcd509
KalyaniBharsat <kalyanibharsat123@gmail.com>
2025-01-16 15:37:23 +0530

    added

--- src/Navbar.js
+++ src/Navbar.js
@@ -2,14 +2,15 @@ import React from 'react';
 
 const NavBar = () => {
   return (
-    <nav className="bg-blue-600 p-4 flex justify-between items-center">
+    <nav className="bg-white-600 p-4 flex justify-between items-center">
       <div className="flex items-center">
+        {/* Make sure the path to the logo image is correct */}
         <img
-          href="Images/Ascentech.png" // Replace this with your company logo
+          src="https://ascentechindia.com/wp-content/uploads/2017/09/ascentech_logo_high_quality1-2.png" // Assuming the image is in the 'public/Images' folder
           alt="Company Logo"
           className="w-10 h-10 mr-3"
         />
-        <h1 className="text-white text-xl font-semibold items-center">ASCENTech Company</h1>
+        <h1 className="text-blue-800 text-xl font-semibold">ASCENTech Company</h1>
       </div>
     </nav>
   );
